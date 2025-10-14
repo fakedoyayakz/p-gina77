6d65ca329c8c85f120c5b5c63d2a0f180d5258ca
fakedoyayakz <haian.costa@escola.pr.gov.br>
2025-10-13 21:56:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let display = document.getElementyId("display");
+let currentInput = ""
+let currentOperator = ""
 
+
+function appendNumber(value){
+    currentInput += value
+    display.textContent = currentInput
+}
+
+function appendOperator(operator){
+    if(currentInput === "" && operator !== ".") return
+    currentInput += operator;
+    display.textContent = currentInput;
+}
+
+function calculate(){
+    try {
+
+    } catch(error){
+
+    }
+}
